937b312d9509d0ef8ca1cfd16610dcb1f63147c0
Daniel Jimenez <daniel@djimenezdev.com>
2023-09-26 11:25:18 -0700

    page content

---
+++ src/pages.ts
@@ -0,0 +1,8 @@
+export const pages = [
+  {
+    content: "",
+  },
+  {
+    content: `Dive into a refreshing blend of sun-kissed fruits, crystal-clear waters, and a hint of coconut breeze. "Enjoy Paradise" isn't just a phrase, it's a sip away. Each glass promises an island escape, where relaxation meets rejuvenation. Cheers to the paradise within!`,
+  },
+];
